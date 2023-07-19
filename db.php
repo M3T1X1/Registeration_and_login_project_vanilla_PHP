328893c05f0acf8897acb9615b4077e8066cdff6
Kacper Dusza <dusza-kacper@wp.pl>
2023-07-19 02:50:35 +0200

    Update on registeration page
    ^

--- db.php
+++ db.php
@@ -7,6 +7,7 @@ $db_name = "cooking_book";
 
 $connect = mysqli_connect($db_hostname,$db_user,$db_password,$db_name);     
 
+/*
 if(isset($connect))
 {
     echo "You are connected";
@@ -16,5 +17,5 @@ else
     echo "Something went wrong with database connection"; 
 }
 
-
+*/
 ?>
---
+++ register_page.php
@@ -0,0 +1,72 @@
+<?php
+include("db.php");
+/*
+foreach($_SERVER as $key => $value)
+{
+    echo $key. "=>" .$value . "<br>";
+}
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+    <h1>Cooking Book</h1>
+    <h2>Welcome to the online cooking book </h2>
+    
+        <form action="<?php  htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post"> 
+        <label>Email:</label><br>
+
+        <input class ="form_input" type = "text" name="email" required><br><br>
+
+        <label>Username:</label><br>
+
+        <input class ="form_input" type = "text" name="username" required><br><br>
+
+        <label> Password:</label><br>
+
+        <input class ="form_input" type = "password" name="password" required><br><br>
+
+        <input id="subbut" type = "submit" name="sub" value = "Register now!">
+    
+    
+        </form>
+        
+        <br>
+
+        <label id="login">
+        Already have an account?
+        <a href="placeholder">Click here to Log in</a>
+        </label>
+    </main>
+</body>
+</html>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    if(isset($_POST["sub"]))
+    {
+        $email = filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
+        $username = filter_input(INPUT_POST,"username",FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST,"password",FILTER_SANITIZE_SPECIAL_CHARS);     
+        
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+        mysqli_query($connect, "INSERT INTO users (email, username, password) VALUES
+                                        ('$email',
+                                            '$username',
+                                                '$password_hash')");
+    
+    }
+}
+mysqli_close($connect);
+?>
